933fcddeb86eec99b1ca2ec96453b161d703a49e
Xwilarg <xwilarg@protonmail.com>
2024-03-12 23:06:18 +0100

    Fix social preview

--- index.php
+++ index.php
@@ -11,9 +11,13 @@ $twig = new Environment($loader);
 $files = glob("articles/*", GLOB_ONLYDIR);
 
 function parseMetadata($path) {
+    $id = basename($path);
+    $metadata = json_decode(file_get_contents("$path/info.json"), true);
+    $metadata["preview"] = "articles/" . $id . "/" . $metadata["preview"];
+
     return [
         "id" => basename($path),
-        "metadata" => json_decode(file_get_contents("$path/info.json"), true)
+        "metadata" => $metadata
     ];
 }
 
@@ -34,6 +38,7 @@ function parseArticle($path) {
 if (isset($_GET["article"]) && in_array("articles/" . $_GET["article"], $files)) {
     echo $twig->render("article.html.twig", [
         "article" => parseArticle("articles/" . $_GET["article"]),
+        "metadata" => parseMetadata("articles/" . $_GET["article"])
     ]);
 } else {
     echo $twig->render("index.html.twig", [
